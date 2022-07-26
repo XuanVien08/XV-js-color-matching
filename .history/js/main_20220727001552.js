78c4baf5fd24d879e041fc4fb2e06f8d15828632
Xuân Viển <xuanvienn123@gmail.com>
2022-07-27 00:28:24 +0700

    1

---
+++ .history/js/main_20220727001552.js
@@ -0,0 +1,39 @@
+import { GAME_STATUS, PAIRS_COUNT } from './constants.js'
+import { getColorElementList, getColorListElement } from './selectors.js'
+import { getRandomColorPairs } from './utils.js'
+// Global variables
+let selections = []
+let gameState = GAME_STATUS.PLAYING
+// TODOs
+// 1. Generating colors using https://github.com/davidmerfield/randomColor
+// 2. Attach item click for all li elements
+// 3. Check win logic
+// 4. Add timer
+// 5. Handle replay click
+
+function initColor() {
+  //random 8 pairs of colors
+  const colorList = getRandomColorPairs(PAIRS_COUNT)
+
+  //bin to li > div.overlay
+  const liList = getColorElementList()
+  liList.forEach((liElement, index) => {
+    const overlayElement = liElement.querySelector('.overlay')
+    if (overlayElement) overlayElement.style.backgroundColor = colorList[index]
+  })
+}
+
+function attachEvenForColorList() {
+  const ulElement = getColorListElement()
+
+  ulElement.addEventListener('click', (event) => {
+    console.log(event.target)
+  })
+}
+
+//MAIN
+;(() => {
+  initColor()
+
+  attachEvenForColorList()
+})()
